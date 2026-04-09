96e0f76c11c3fdd13e422f45c6125974e4e77f55
Hexsa-web <hexsarizkyrizky@gmail.com>
2026-04-09 09:00:41 +0700

    update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,13 +7,19 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
+    ->withMiddleware(function (Middleware $middleware) {
+
+        // === TAMBAHKAN BAGIAN INI ===
+        $middleware->alias([
+            'admin' => \App\Http\Middleware\Admin::class,
+            // tambahkan middleware lain kalau ada, contoh:
+            // 'is_user' => \App\Http\Middleware\IsUser::class,
+        ]);
+
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
